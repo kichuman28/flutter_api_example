2a3c2f5554281062833a14ef8ef7c8fd8a5bb3c9
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-07 15:45:12 +0530

    Update: Changed textfield border to green

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -56,7 +56,14 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.only(left: 150.0, right: 150.0),
               child: TextField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    borderSide: BorderSide(color: Colors.green, width: 3.0),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    borderSide: BorderSide(color: Colors.green, width: 3.0),
+                  ),
                   hintText: 'User ID',
                 ),
                 controller: myController,
