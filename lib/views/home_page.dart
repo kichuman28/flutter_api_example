99afcbd404e7e60ef881ddc589b64773279f1262
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-07 15:40:05 +0530

    Update: Removed useless stuff

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,9 +5,7 @@ import 'package:counter/services/user_service.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -50,10 +48,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
