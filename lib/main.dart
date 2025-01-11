05443cc7dd58795ed2474db45c746bc22c05aa33
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-11 07:45:12 +0530

    Fix: Removed useless code

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,6 @@ class _MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: MyHomePage(),
     );
   }
