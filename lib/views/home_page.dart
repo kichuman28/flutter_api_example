619f0fb0519ee8fafd1200ecec71dfa950811248
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-14 18:45:01 +0530

    Update: Used UserProvider in home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,8 +1,8 @@
 // Created by: Adwaith Jayasankar, Created at: 06-01-2025 22:47
 import 'package:counter/components/actor_card.dart';
-import 'package:counter/models/user_model.dart';
-import 'package:counter/services/user_service.dart';
+import 'package:counter/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../enums/api_status_enum.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -13,8 +13,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  UserModel? info;
-  Status apiStatus = Status.initial;
   late TextEditingController _textFieldController;
   OutlineInputBorder textFieldColor = OutlineInputBorder(
     borderSide: const BorderSide(color: Colors.green, width: 3.0),
@@ -34,64 +32,49 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  void getData(String userID) async {
-    setState(() {
-      apiStatus = Status.loading;
-    });
-
-    info = await UserService.getUser(userID);
-    if (info != null && info!.success) {
-      setState(() {
-        apiStatus = Status.success;
-      });
-    } else {
-      setState(() {
-        apiStatus = Status.error;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 150.0, right: 150.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: textFieldColor,
-                  focusedBorder: textFieldColor,
-                  hintText: 'User ID',
+    return Consumer<UserProvider>(
+      builder: (context, userModel, child) => Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 150.0, right: 150.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: textFieldColor,
+                    focusedBorder: textFieldColor,
+                    hintText: 'User ID',
+                  ),
+                  controller: _textFieldController,
                 ),
-                controller: _textFieldController,
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all(Colors.blue),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.blue),
+                ),
+                onPressed: () {
+                  userModel.getData(_textFieldController.text);
+                },
+                child: Text(
+                  "Fetch User",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              onPressed: () {
-                getData(_textFieldController.text);
-              },
-              child: Text(
-                "Fetch User",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 20),
-            if (apiStatus == Status.loading)
-              const CircularProgressIndicator()
-            else if (apiStatus == Status.initial)
-              const Text("Enter a user ID and click to get the user details")
-            else if (apiStatus == Status.success)
-              ActorCard(info: info)
-            else if (apiStatus == Status.error)
-              Text(info?.error ?? 'An unexpected error occurred'),
-          ],
+              SizedBox(height: 20),
+              if (userModel.status == Status.loading)
+                const CircularProgressIndicator()
+              else if (userModel.status == Status.initial)
+                const Text("Enter a user ID and click to get the user details")
+              else if (userModel.status == Status.success)
+                ActorCard(info: userModel.info)
+              else if (userModel.status == Status.error)
+                Text(userModel.info?.error ?? 'An unexpected error occurred'),
+            ],
+          ),
         ),
       ),
     );
